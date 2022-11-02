3a226830f6102bac0f390ee36afd1968f2ab91d1
NIIH3 <117304630+NIIH3@users.noreply.github.com>
2022-11-02 19:32:31 +0100

    Add files via upload

---
+++ Ejercicios4/src/Ejercicio13FOR.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio13FOR {
+
+	public static void main(String[] args) {
+		double num, contadorP, sumaP, contadorN, sumaN, contador0;
+		
+		
+		
+	}
+
+}
